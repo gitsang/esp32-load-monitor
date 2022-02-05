d200967e506e29a07273319bbb9b87b9d035ca22
gitsang <sang.chen@outlook.com>
2022-02-05 11:54:12 +0000

    fix: error solve

--- main.go
+++ main.go
@@ -21,21 +21,38 @@ func main() {
 			cpuloads, _ := cpu.Percent(time.Duration(time.Second), false)
 			cpuload := cpuloads[0]
 			cpu8bit := cpuload / 100 * 256
-			resp, _ := http.Get(fmt.Sprintf("http://%s:%d/left/%f", esp32host, 80, cpu8bit))
-			body, _ := ioutil.ReadAll(resp.Body)
-			log.Println("cpuload: ", cpuload, ", cpu8bit: ", cpu8bit, ", resp: ", string(body))
+			resp, err := http.Get(fmt.Sprintf("http://%s:%d/left/%f", esp32host, 80, cpu8bit))
+			if err != nil {
+				log.Println("[error] send cpu load failed")
+				continue
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("[error] http read body failed")
+				continue
+			}
+			log.Println("[info] cpuload: ", cpuload, ", cpu8bit: ", cpu8bit, ", resp: ", string(body))
 		}
 	}()
 
 	go func() {
 		for {
+			time.Sleep(time.Second)
+
 			memInfo, _ := mem.VirtualMemory()
 			memload := memInfo.UsedPercent
 			mem8bit := memload / 100 * 256
-			resp, _ := http.Get(fmt.Sprintf("http://%s:%d/right/%f", esp32host, 80, mem8bit))
-			body, _ := ioutil.ReadAll(resp.Body)
-			log.Println("memload: ", memload, ", mem8bit: ", mem8bit, ", resp: ", string(body))
-			time.Sleep(time.Second)
+			resp, err := http.Get(fmt.Sprintf("http://%s:%d/right/%f", esp32host, 80, mem8bit))
+			if err != nil {
+				log.Println("[error] send mem load failed")
+				continue
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Println("[error] http read body failed")
+				continue
+			}
+			log.Println("[info] memload: ", memload, ", mem8bit: ", mem8bit, ", resp: ", string(body))
 		}
 	}()
 
