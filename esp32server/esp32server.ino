c9e12f343ae3ec89d15d0fc8e835e6359ef99f14
gitsang <sang.chen@outlook.com>
2022-03-30 23:44:35 +0800

    feat: esp32 support ddns

--- esp32server/esp32server.ino
+++ esp32server/esp32server.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+#include <HTTPClient.h>
 
 #include <uri/UriBraces.h>
 #include <uri/UriRegex.h>
@@ -23,11 +24,18 @@ String RKey = "right";
 String LValue = "255";
 String RValue = "100";
 
-String Domain = "loadoutput.dns.army"
-String Token = "nDZTHRftjo29LTZKtUq7jGk1bTdAxM"
+String Domain = "loadoutput.dns.army";
+String Token = "nDZTHRftjo29LTZKtUq7jGk1bTdAxM";
 
 // Web Server
 WebServer server(80);
+
+String IpAddress2String(const IPAddress& ipAddress) {
+    return String(ipAddress[0]) + String(".") +
+           String(ipAddress[1]) + String(".") +
+           String(ipAddress[2]) + String(".") +
+           String(ipAddress[3]);
+}
 
 void setup(void) {
   // Init WIFI
@@ -38,18 +46,12 @@ void setup(void) {
   }
 
   // Init DDNS
-  String url = String("http://dynv6.com/api/update?hostname=") + Domain + "&token=" + Token + "&ipv4=" + WiFi.localIP();
-  http.begin(url)
+  HTTPClient http;
+  String ip = IpAddress2String(WiFi.localIP());;
+  String url = String("http://dynv6.com/api/update?hostname=") + Domain + "&token=" + Token + "&ipv4=" + ip;
+  http.begin(url);
   int httpCode = http.GET(); //Make the request
-
-  if (httpCode > 0) { //Check for the returning code
-    String payload = http.getString();
-    Serial.println(httpCode);
-    Serial.println(payload);
-  } else {
-    Serial.println("Error on HTTP request");
-  }
-
+  String payload = http.getString();
   http.end(); //Free the resources
 
   // Init PWM PIN
@@ -75,8 +77,10 @@ void setup(void) {
     server.send(200, "text/plain", "Right: " + RValue + "/255");
   });
 
-  server.on(UriBraces("/ip"), []() {
-    server.send(200, "text/plain", WiFi.localIP());
+  server.on(UriBraces("/test"), []() {
+    String ip = IpAddress2String(WiFi.localIP());;
+    String url = String("http://dynv6.com/api/update?hostname=") + Domain + "&token=" + Token + "&ipv4=" + ip;
+    server.send(200, "text/plain", url);
   });
 
   server.begin();
